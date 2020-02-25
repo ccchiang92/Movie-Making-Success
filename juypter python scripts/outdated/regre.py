95e4df296a3a330cc812f2cb3859e0250af902d9
Chris Chiang <christopherchiang92@gmail.com>
2020-02-24 23:13:25 -0800

    comments readme and presentation

--- juypter python scripts/outdated/regre.py
+++ juypter python scripts/outdated/regre.py
@@ -1,9 +1,9 @@
-import scipy.stats
+import scipy.stats as st
 import matplotlib.pyplot as plt
 import numpy as np
 def line_plot(x, y):
     # Function that takes in 2 panda series x, y and plots a linear regression line
-    slope, inter, rval, pval, stderr = scipy.stats.linregress(x, y)
+    slope, inter, rval, pval, stderr = st.linregress(x, y)
     print(f'The r-squared is : {rval ** 2}')
     x_line = np.arange(x.min(), x.max()+1, 1)
     y_line = (slope * x_line) + inter
